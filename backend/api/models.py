44954fd5a331ca15edea755064202b2e7cac620d
Didar <didarkuliew54@gmail.com>
2025-02-21 18:30:22 +0500

    User & Profile Models

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,61 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.db.models.signals import post_save
+from django.utils.text import slugify
+from shortuuid.django_fields import ShortUUIDField
+import shortuuid
 
-# Create your models here.
+class User(AbstractUser):
+    username = models.CharField(unique=True, max_length=100)
+    email = models.EmailField(unique=True)
+    full_name = models.CharField(max_length=100, null=True, blank=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def save(self, *args, **kwargs):
+        email_username, mobile = self.email.split('@')
+        if not self.full_name:
+            self.full_name = email_username
+        if not self.username:
+            self.username = email_username
+
+        super(User, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.username
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='image', default='default/user.jpg')
+    full_name = models.CharField(max_length=100)
+    bio = models.CharField(max_length=100, null=True, blank=True)
+    about = models.CharField(max_length=100, null=True, blank=True)
+    author = models.BooleanField(default=False)
+    country = models.CharField(max_length=100, null=True, blank=True)
+    facebook = models.CharField(max_length=100, null=True, blank=True)
+    instagram = models.CharField(max_length=100, null=True, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+    update = models.DateField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        if not self.full_name:
+            self.full_name = self.user.full_name
+        super(Profile, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.user.username
+    
+
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+
+post_save.connect(create_user_profile, sender=User)
+post_save.connect(save_user_profile, sender=User)
